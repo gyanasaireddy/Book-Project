796638c593fb937333180924b608253f24cebaf8
Gyana Sai Reddy <gjnanasai@student.nitw.ac.in>
2025-07-07 17:39:09 +0530

    Integrated database with front end

--- index.js
+++ index.js
@@ -13,38 +13,56 @@ const db = new pg.Client({
   password: "generatorx",
   host: "localhost",
   port: 5432,
-  database: "book_notes",
+  database: "book_project",
 });
 
 db.connect();
 
-const GOOGLE_BOOKS_API = "https://www.googleapis.com/books/v1/volumes";
-// const API_KEY = process.env.GOOGLE_BOOKS_KEY;
-const API_KEY = "AIzaSyCZAcv-6nJkrWsT05DfSQAJ-Ch9oVcEt70";
+let dbBook = [];
+let titleList = ["atomic habits", "deep work"];
 
-app.get("/", async (req, res) => {
-  let titleList = ["atomic habits", "deep work"];
-  // if (!q) return res.status(400).json({ error: "Missing query parameter q" });
+async function getDatabase() {
+  titleList = []
+  const result = await db.query('SELECT * FROM books')
+  console.log(result.rows[0]);
+  dbBook =  result.rows;
+  result.rows.forEach((b)=>{
+    titleList.push(b.title)
+  })
+  
+}
 
-  try {
-    const requests = titleList.map((title) =>
+let books =[];
+
+async function getAPIBooks() {
+      const requests = titleList.map((title) =>
       axios.get(GOOGLE_BOOKS_API, {
         params: {
           q: title,
           key: API_KEY,
           maxResults: 1,
-          fields: "items(volumeInfo(title,authors,description,imageLinks))",
+          fields: "items(volumeInfo(title,authors,description,imageLinks,industryIdentifiers))",
         },
       })
     );
 
     const results = await Promise.all(requests);
-    const books = results
+    return  results
       .map((r) => r.data.items?.[0] || null)
       .filter((b) => b !== null);
+  
+}
+const GOOGLE_BOOKS_API = "https://www.googleapis.com/books/v1/volumes";
+// const API_KEY = process.env.GOOGLE_BOOKS_KEY;
+const API_KEY = "AIzaSyCZAcv-6nJkrWsT05DfSQAJ-Ch9oVcEt70";
 
-    res.render('index.ejs',{books})
-    console.log(books);
+app.get("/", async (req, res) => {
+  // if (!q) return res.status(400).json({ error: "Missing query parameter q" });
+  await getDatabase()
+  try {
+    books = await getAPIBooks();
+    res.render('index.ejs',{books:books, database:dbBook})
+    // console.log(books);
     
   } catch (err) {
     console.error("Google Books API error:", err.message);
@@ -52,8 +70,21 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.get("/readmore", (req, res) => {
-  res.render("readmore.ejs");
+app.get("/readmore/:id", (req, res) => {
+  // console.log(req.query.title);
+  // console.log(books);
+  
+  const isbn = req.params.id
+  
+  const i = books.findIndex((book)=>
+    book.volumeInfo.industryIdentifiers[0].identifier == isbn
+ )
+  
+  // console.log(books[i]+ '----------------'+dbBook[i].rating);
+  
+
+
+  res.render("readmore.ejs",{selectedBook:books[i], database:dbBook[i] });
 });
 
 app.listen(port, () => {
